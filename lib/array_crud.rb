41efee3338e1213ad0fc022f5714d1257d5a5593
Benjamin Ebomwonyi <bebomwonyi@gmail.com>
2020-07-03 13:54:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,12 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array=[1, 2, 3, 4]
+  create_an_array=['1', '2', '3', '4']
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [1, 2, 3, 4]
-  array.push (5)
+  array = ['1', '2', '3', '4']
+  array.push ('5')
 end
 
 def add_element_to_start_of_array(array, element)
